e9d8465269896e797cdadecc04e369faf4e4908b
chriss-inno <chriss.innocent@outlook.com>
2017-02-09 03:35:25 +0300

    Update padrietric addition of validation

--- protected/resources/views/assessments/paediatric/create.blade.php
+++ protected/resources/views/assessments/paediatric/create.blade.php
@@ -12,7 +12,6 @@
 <script type="text/javascript" src="{{asset("assets/js/plugins/ui/ripple.min.js")}}"></script>
 <script type="text/javascript" src="{{asset("assets/js/plugins/tables/datatables/datatables.min.js")}}"></script>
 <script type="text/javascript" src="{{asset("assets/js/plugins/forms/selects/select2.min.js")}}"></script>
-<script type="text/javascript" src="{{asset("assets/js/core/app.js")}}"></script>
 <script type="text/javascript" src="{{asset("assets/js/plugins/ui/ripple.min.js")}}"></script>
 <script type="text/javascript" src="{{asset("assets/js/pages/wizard_form.js")}}"></script>
 <script>
@@ -35,7 +34,7 @@
 
 
         // Basic datatable
-        $('.datatable-basic').DataTable({
+        $('.datatable-select-clients').DataTable({
             "scrollX": false,
             ajax: '{{url('getwaclientsjson')}}', //this url load JSON Client details to reduce loading time
             "fnDrawCallback": function (oSettings) {
@@ -79,7 +78,7 @@
             <div class="form-group">
                 <div class="row clearfix">
                     <div class="col-md-12 column">
-                        <table class="table datatable-basic table-bordered table-hover" id="tab_logic">
+                        <table class="table datatable-select-clients table-bordered table-hover" id="tab_logic">
                             <thead>
                             <tr >
                                 <th class="text-center">
@@ -554,7 +553,56 @@
             formPluginEnabled: true,
             disableInputFields: false,
             inDuration: 150,
-            outDuration: 150,
+            outDuration: 150,ignore: 'input[type=hidden], .select2-search__field', // ignore hidden fields
+            errorClass: 'validation-error-label',
+            successClass: 'validation-valid-label',
+            highlight: function(element, errorClass) {
+                $(element).removeClass(errorClass);
+            },
+            unhighlight: function(element, errorClass) {
+                $(element).removeClass(errorClass);
+            },
+
+            // Different components require proper error label placement
+            errorPlacement: function(error, element) {
+
+                // Styled checkboxes, radios, bootstrap switch
+                if (element.parents('div').hasClass("checker") || element.parents('div').hasClass("choice") || element.parent().hasClass('bootstrap-switch-container') ) {
+                    if(element.parents('label').hasClass('checkbox-inline') || element.parents('label').hasClass('radio-inline')) {
+                        error.appendTo( element.parent().parent().parent().parent() );
+                    }
+                    else {
+                        error.appendTo( element.parent().parent().parent().parent().parent() );
+                    }
+                }
+
+                // Unstyled checkboxes, radios
+                else if (element.parents('div').hasClass('checkbox') || element.parents('div').hasClass('radio')) {
+                    error.appendTo( element.parent().parent().parent() );
+                }
+
+                // Input with icons and Select2
+                else if (element.parents('div').hasClass('has-feedback') || element.hasClass('select2-hidden-accessible')) {
+                    error.appendTo( element.parent() );
+                }
+
+                // Inline checkboxes, radios
+                else if (element.parents('label').hasClass('checkbox-inline') || element.parents('label').hasClass('radio-inline')) {
+                    error.appendTo( element.parent().parent() );
+                }
+
+                // Input group, styled file input
+                else if (element.parent().hasClass('uploader') || element.parents().hasClass('input-group')) {
+                    error.appendTo( element.parent().parent() );
+                }
+
+                else {
+                    error.insertAfter(element);
+                }
+            },
+            rules: {
+                client_id:"required",
+            },
             formOptions :{
                 success: function(data){
                     swal({title: "Form Submitted successful!", text: "Vulnerability form is now saved! ", type: "success", timer: 2000, confirmButtonColor: "#43ABDB"})
@@ -567,6 +615,10 @@
                 resetForm: true
             }
         });
+    });
+    // Update single file input state
+    $(".form-validation").on("step_shown", function(event, data) {
+        $.uniform.update();
     });
 </script>
 <!-- /submit form with AJAX -->
