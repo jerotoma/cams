635438a0a7acc086279f16660aee3d9bafa51ea6
jrm-shirima <erominimatei@gmail.com>
2017-02-19 23:24:21 +0300

    updates

--- protected/resources/views/referrals/create.blade.php
+++ protected/resources/views/referrals/create.blade.php
@@ -382,16 +382,9 @@
                                     </select>
                                 </div>
                             </div>
-
-
-                        </div>
-
-
+                  </div>
 
                </fieldset>
-
-
-
 
                     <fieldset class="scheduler-border">
                         <legend class="text-bold"><h3 class="text-left text-bold">Background Information/Reason for Referral:
@@ -419,18 +412,20 @@
 
                     <fieldset class="scheduler-border">
                         <legend class="text-bold"><h3 class="text-left text-bold">Services Requested   </h3></legend>
+                        <div class="row">
+                            <div class="col-sm-4g">
+                                <label class="checkbox-inline">
+                                    <input type="checkbox" class="styled">
+                                    Remember
+                                </label>
+                            </div>
+                        </div>
 
                         <div class="row">
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <div class="form-group ">
-
-                                        <option value="mental_services">Mental Health Services </option>
-
-                                    <label class="control-label">Has the client been informed of the referral?   </label>
-                                      <textarea  class="form-control" name="clientinfo_referral" id="clientinfo_referral"></textarea>
-                                    </div>
                                     <select name="service_request"  multiple="multiple" class="select" >
-
+                                        <option value="mental_services">Mental Health Services </option>
                                         <option value="protection_services">Protection Support/ Services </option>
                                         <option value="shelter">Shelter</option>
                                         <option value="psychological">Psychological Interventions </option>
@@ -445,24 +440,10 @@
                                         <option value="education">Education </option>
                                         <option value="psychocial_activities">Psychosocial Activities </option>
                                     </select>
+                                   </div>
+
                                 </div>
                             </div>
-                            <div class="col-md-6">
-                                <div class="form-group ">
-                                    <label class="control-label">Has the client been referred to any other organizations?   </label>
-                                    <textarea  class="form-control" name="referal_other_org" id="referal_other_org"></textarea>
-                                </div>
-                            </div>
-
-
-
-                        </div>
-
-
-
-
-
-
                     </fieldset>
 
 
@@ -473,16 +454,7 @@
                 </fieldset>
 
 
-                <div class="row">
-                    <div class="col-md-8 col-sm-8 pull-left" id="output">
 
-                    </div>
-                    <div class="col-md-4 col-sm-4 pull-right text-right">
-                        <button type="button" class="btn btn-danger "  data-dismiss="modal">Cancel</button>
-                        <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> Save </button>
-                    </div>
-
-                </div>
             </div>
         </div>
         {!! Form::close() !!}
@@ -512,49 +484,49 @@
         ignore: 'input[type=hidden], .select2-search__field', // ignore hidden fields
         errorClass: 'validation-error-label',
         successClass: 'validation-valid-label',
-        highlight: function(element, errorClass) {
+        highlight: function (element, errorClass) {
             $(element).removeClass(errorClass);
         },
-        unhighlight: function(element, errorClass) {
+        unhighlight: function (element, errorClass) {
             $(element).removeClass(errorClass);
         },
-        errorPlacement: function(error, element) {
+        errorPlacement: function (error, element) {
 
             // Styled checkboxes, radios, bootstrap switch
-            if (element.parents('div').hasClass("checker") || element.parents('div').hasClass("choice") || element.parent().hasClass('bootstrap-switch-container') ) {
-                if(element.parents('label').hasClass('checkbox-inline') || element.parents('label').hasClass('radio-inline')) {
-                    error.appendTo( element.parent().parent().parent().parent() );
+            if (element.parents('div').hasClass("checker") || element.parents('div').hasClass("choice") || element.parent().hasClass('bootstrap-switch-container')) {
+                if (element.parents('label').hasClass('checkbox-inline') || element.parents('label').hasClass('radio-inline')) {
+                    error.appendTo(element.parent().parent().parent().parent());
                 }
                 else {
-                    error.appendTo( element.parent().parent().parent().parent().parent() );
+                    error.appendTo(element.parent().parent().parent().parent().parent());
                 }
             }
 
             // Unstyled checkboxes, radios
             else if (element.parents('div').hasClass('checkbox') || element.parents('div').hasClass('radio')) {
-                error.appendTo( element.parent().parent().parent() );
+                error.appendTo(element.parent().parent().parent());
             }
 
             // Input with icons and Select2
             else if (element.parents('div').hasClass('has-feedback') || element.hasClass('select2-hidden-accessible')) {
-                error.appendTo( element.parent() );
+                error.appendTo(element.parent());
             }
 
             // Inline checkboxes, radios
             else if (element.parents('label').hasClass('checkbox-inline') || element.parents('label').hasClass('radio-inline')) {
-                error.appendTo( element.parent().parent() );
+                error.appendTo(element.parent().parent());
             }
 
             // Input group, styled file input
             else if (element.parent().hasClass('uploader') || element.parents().hasClass('input-group')) {
-                error.appendTo( element.parent().parent() );
+                error.appendTo(element.parent().parent());
             }
 
             else {
                 error.insertAfter(element);
             }
         },
-        errorElement:'div',
+        errorElement: 'div',
         rules: {
             ref_org: "required",
             rec_org: "required",
@@ -567,43 +539,44 @@
             client_number: "required",
 
 
-
         },
         messages: {
             ref_org: "Please this field is required",
             rec_org: "Please this field is required",
             client_language: "Please field is required",
 
-            client_age:{
-                number:"Please enter valid age",
-            } ,
-
-
+            client_age: {
+                number: "Please enter valid age",
             },
 
-        },
-        submitHandler: function(form) {
+        }, submitHandler: function (form) {
             $("#output").html("<h3><span class='text-info'><i class='fa fa-spinner fa-spin'></i> Making changes please wait...</span><h3>");
             var postData = $('#formClients').serializeArray();
             var formURL = $('#formClients').attr("action");
             $.ajax(
                 {
-                    url : formURL,
+                    url: formURL,
                     type: "POST",
-                    data : postData,
-                    success: function(data){
-                        swal({title: "Form Submitted successful!", text: data.message, type: "success", timer: 2000, confirmButtonColor: "#43ABDB"})
-                        setTimeout(function() {
-                            location.replace("{{url('referrals')}}");
+                    data: postData,
+                    success: function (data) {
+                        swal({
+                            title: "Form Submitted successful!",
+                            text: data.message,
+                            type: "success",
+                            timer: 2000,
+                            confirmButtonColor: "#43ABDB"
+                        })
+                        setTimeout(function () {
+                            location.replace("{{url('clients')}}");
                             $("#output").html("");
                         }, 2000);
                     },
-                    error: function(jqXhr,status, response) {
+                    error: function (jqXhr, status, response) {
                         console.log(jqXhr);
-                        if( jqXhr.status === 401 ) {
+                        if (jqXhr.status === 401) {
                             location.replace('{{url('login')}}');
                         }
-                        if( jqXhr.status === 400 ) {
+                        if (jqXhr.status === 400) {
                             var errors = jqXhr.responseJSON.errors;
                             errorsHtml = '<div class="alert alert-danger"><p class="text-uppercase text-bold">There are errors kindly check</p><ul>';
                             $.each(errors, function (key, value) {
@@ -612,8 +585,7 @@
                             errorsHtml += '</ul></di>';
                             $('#output').html(errorsHtml);
                         }
-                        else
-                        {
+                        else {
                             $('#output').html(jqXhr.message);
                         }
 
@@ -621,6 +593,7 @@
                 });
         }
     });
+
     $("#civil_status").change(function () {
         var id1 = this.value;
         if(id1 != "Married")
