0ea3352c11107a3dca24dbb2f90902b03acbdca7
jrm-shirima <erominimatei@gmail.com>
2017-02-20 02:47:00 +0300

    Update refarralfor

--- protected/resources/views/referrals/show.blade.php
+++ protected/resources/views/referrals/show.blade.php
@@ -17,116 +17,228 @@
             <h4><strong>
                     INTER – AGENCY REFERRAL FORM
                 </strong></h4>
-            <h5><strong>
-                    Progress Number: {{$referral->progress_number}}
-                </strong></h5>
         </div>
     </div>
     <div class="row" style="margin-top: 10px">
         <div class="col-md-12 ">
+            <h5> Referring agency coy</h5>
             <table class="table table-bordered">
                 <tr>
-                    <th class="col-md-2">Case name: </th>
-                    <td class="col-md-10" colspan="3">{{$referral->case_name}} </td>
+                    <th style="background-color:#ccc">Referring Agency </th>
                 </tr>
                 <tr>
-                    <th class="col-md-2">Date: </th>
-                    <td class="col-md-3">{{$referral->referral_date}} </td>
-                    <th class="col-md-2">Completed by: </th>
-                    <td class="col-md-5">{{$referral->completed_by}} </td>
+                    <th>Agency /Org</th>
+                    <td></td>
+                    <th>Contact</th>
+                    <td></td>
                 </tr>
                 <tr>
-                    <th class="col-md-3">Name of Client Concerned: </th>
-                    <td class="col-md-9" colspan="3">@if(is_object($referral->client) && $referral->client != null){{$referral->client->full_name}} @endif</td>
+                    <th>Phone</th>
+                    <td></td>
+                    <th>Email</th>
+                    <td></td>
                 </tr>
                 <tr>
-                    <th class="col-md-3">Location the referral originated:  </th>
-                    <td class="col-md-9" colspan="3">{{$referral->location}}</td>
-                </tr>
-                <tr>
-                    <th class="col-md-3">Age: </th>
-                    <th class="col-md-3">Disabilities: </th>
-                    <th class="col-md-3">Date of birth: </th>
-                    <th class="col-md-3">Sex: </th>
-                </tr>
-                <tr>
-                    <td class="col-md-3"> {{$referral->age}}</td>
-                    <td class="col-md-3">{{$referral->disabilities}} </td>
-                    <td class="col-md-3">{{date("d-M-Y", strtotime($referral->birth_date))}} </td>
-                    <td class="col-md-3">@if(is_object($referral->client) && $referral->client != null){{$referral->client->sex}} @endif</td>
-                </tr>
-                <tr>
-                    <th class="col-md-3"> Ethnic Background</th>
-                    <td class="col-md-9" colspan="3">{{$referral->ethnic_background}} </td>
-                </tr>
-                <tr>
-                    <th class="col-md-3"> Contact <small></small></th>
-                    <td class="col-md-3" >{{$referral->contact}} </td>
-                    <th class="col-md-3"> Telephone number:</th>
-                    <td class="col-md-3" >{{$referral->phone}} </td>
-                </tr>
-                <tr>
-                    <th class="col-md-3"> Name of Person Who Originated concern and contact details: </th>
-                    <td class="col-md-9" colspan="3">{{$referral->person_name }}</td>
-                </tr>
-                <tr>
-                    <th class="col-md-3"> Relationship to client: </th>
-                    <td class="col-md-3" >{{$referral->relationship }}</td>
-                    <th class="col-md-3"> Approached for assistance with plot/address: </th>
-                    <td class="col-md-3" >{{$referral->person_name_address }}</td>
-                </tr>
-                <tr>
-                    <th class="col-md-6">Consent Obtained to Share Information: </th>
-                    <td class="col-md-6" colspan="3">{{$referral->consent}} </td>
-                </tr>
-                <tr>
-                    <th class="col-md-6">Parental Consent provided if Client is Under 18 years of Age: </th>
-                    <td class="col-md-6" colspan="3">{{$referral->parental_consent}} </td>
-                </tr>
-                <tr>
-                    <th class="col-md-6">Any Attachment included:  </th>
-                    <td class="col-md-6" colspan="3">{{$referral->attachment}} </td>
-                </tr>
-                <tr>
-                    <th class="col-md-6">Initial Action Recommended or Taken: </th>
-                    <td class="col-md-6" colspan="3"><?php echo $referral->initial_action; ?></td>
-                </tr>
-                <tr>
-                    <th class="col-md-6">Timeframes agreed/proposed: </th>
-                    <td class="col-md-6" colspan="3"><?php echo $referral->time_frames; ?></td>
-                </tr>
-                <tr>
-                    <th class="col-md-12" colspan="4">Additional Comments Regarding Concern [any information volunteered by client]:  </th>
+                    <th>Location</th>
+                    <td></td>
 
                 </tr>
+            </table>
+
+            <h5> Receiving Agency</h5>
+            <table class="table table-bordered">
                 <tr>
-                    <td class="col-md-12" colspan="4"><?php echo $referral->additional_comments; ?> </td>
+                    <th style="background-color:#ccc">Receiving Agency </th>
                 </tr>
                 <tr>
-                    <th class="col-md-3">Primary Concern: </th>
-                    <td class="col-md-9" colspan="3"><?php echo $referral->primary_concern; ?></td>
+                    <th>Agency /Org</th>
+                    <td></td>
+                    <th>Contact</th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Phone</th>
+                    <td></td>
+                    <th>Email</th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Location</th>
+                    <td></td>
+
+                </tr>
+            </table>
+
+            <h5> Client Information</h5>
+            <table class="table table-bordered">
+                <tr>
+                    <th style="background-color:#ccc">Client Information </th>
+                </tr>
+                <tr>
+                    <th>Name</th>
+                    <td></td>
+                    <th>Phone</th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Address</th>
+                    <td></td>
+                    <th>Age</th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Sex</th>
+                    <td></td>
+                    <th>Nationality</th>
+                    <td></td>
+                <tr>
+                    <th>Language</th>
+                    <td></td>
+                    <th>ID numbers</th>
+                    <td></td>
+
+                </tr>
+            </table>
+            <h5> If Client Is a Minor (under 18 years)</h5>
+            <table class="table table-bordered">
+                <tr>
+                    <th style="background-color:#ccc">If Client Is a Minor (under 18 years) </th>
+                </tr>
+                <tr>
+                    <th>Name of primary caregiver:</th>
+                    <td></td>
+                    <th>Relationship to child:</th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Contact information for caregiver</th>
+                    <td></td>
+                    <th>Is child separated or unaccompanied?  </th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Caregiver is informed of referral?  </th>
+                    <td></td>
+
+                </tr>
+            </table>
+
+            <h5> Background Information/Reason for Referral:
+                (problem description, duration, frequency, etc.) and Services Already Provided
+            </h5>
+            <table class="table table-bordered">
+                <tr>
+                    <th style="background-color:#ccc"> Background Information/Reason for Referral:
+                        (problem description, duration, frequency, etc.) and Services Already Provided
+                    </th>
+                </tr>
+                <tr>
+                    <th>Has the client been informed of the referral?  :</th>
+                    <td></td>
+                    <th>Has the client been referred to any other organizations?  </th>
+                    <td></td>
                 </tr>
 
             </table>
+
+            <h5> Services Requested </h5>
             <table class="table table-bordered">
                 <tr>
-                    <th class="col-md-3">Referred to:  </th>
-                    <td class="col-md-3" >{{$referral->referred_to}} </td>
-                    <th class="col-md-3">Position:  </th>
-                    <td class="col-md-3" >{{$referral->referred_to_position}} </td>
+                    <th style="background-color:#ccc"> Services Requested  </th>
                 </tr>
                 <tr>
-                    <th class="col-md-3">Organization/ Institution: </th>
-                    <td class="col-md-9" colspan="3">{{$referral->organization}} </td>
+                    <th>Mental Health Services</th>
+                    <td></td>
+                    <th>Protection Support/ Services   </th>
+                    <td></td>
+                    <th>Shelter</th>
+                    <td></td>
                 </tr>
                 <tr>
-                    <th class="col-md-3">Telephone number: </th>
-                    <td class="col-md-3">{{$referral->org_phone}} </td>
-                    <th class="col-md-3">Email address: </th>
-                    <td class="col-md-3">{{$referral->org_email}} </td>
+                    <th>Psychological Interventions </th>
+                    <td></td>
+                    <th>Community Centre/ Social Services  </th>
+                    <td></td>
+                    <th>Material Assistance</th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Physical Health Care  </th>
+                    <td></td>
+                    <th>Family Tracing Services   </th>
+                    <td></td>
+                    <th>Nutrition</th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Physical Rehabilitation   </th>
+                    <td></td>
+                    <th>Legal Assistance   </th>
+                    <td></td>
+                    <th> Financial Assistance  </th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Physical Rehabilitation   </th>
+                    <td></td>
+                    <th>Legal Assistance   </th>
+                    <td></td>
+                    <th> Financial Assistance  </th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Psychosocial Activities </th>
+                    <td></td>
+                    <th>Education </th>
+                    <td></td>
+                    <th> Please explain any requested services:  </th>
+                    <td></td>
                 </tr>
 
             </table>
+
+            <h5> Consent to Release Information (Read with client/ caregiver and answer any questions before s/he signs below)</h5>
+            <table class="table table-bordered">
+                <tr>
+                    <th style="background-color:#ccc">Consent to Release Information (Read with client/ caregiver and answer any questions before s/he signs below) </th>
+                </tr>
+                <tr>
+                    <th>I,.............................................................(client name), understand that the purpose of the referral and of disclosing this information to                                        (receiving agency) is to ensure the safety and continuity of care among service providers seeking to serve the client. The service provider,                                        (referring agency), has clearly explained the procedure of the referral to me and has listed the exact information that is to be disclosed. By signing this form,
+                        I authorize this exchange of information:</th>
+
+                    <th>Signature of Responsible Party:</th>
+                    <td></td>
+                </tr>
+
+            </table>
+            <h5> Details of Referral</h5>
+
+            <table class="table table-bordered">
+                <tr>
+                    <th style="background-color:#ccc"> Details of Referral</th>
+                </tr>
+                <tr>
+                    <th>Any Contact or other restriction :</th>
+                    <td></td>
+                    <th>Referral delivered via   Phone (Emegency Only);Email;Electronically(App or datbase; </th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Followup Expected Via Phone ;Email ; in Person by Date(DD/MM/YY);</th>
+                    <td></td>
+                    <th>Intermation agencies Agree to exchange in followup: </th>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th>Name and Signature of  receipient</th>
+                    <td></td>
+                    <th>Date Received(DD/MM/YY) </th>
+                    <td></td>
+                </tr>
+
+            </table>
+
+
 
         </div>
     </div>
