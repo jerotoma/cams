664cd384b2d6a51af36444966b3c90661c37007a
chriss-inno <chriss.innocent@outlook.com>
2017-05-04 18:25:05 +0300

    Update pdf fetch link

--- protected/config/snappy.php
+++ protected/config/snappy.php
@@ -5,16 +5,16 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        'binary'  => "/usr/local/wkhtmlto/bin/wkhtmltopdf", //For linux
-        //'binary'  => "\"C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe\"", //For windows remove comments
+        //'binary'  => "/usr/local/wkhtmlto/bin/wkhtmltopdf", //For linux remove comments nd add coments to bellow line
+        'binary'  => "\"C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe\"", //For windows remove comments
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
     ),
     'image' => array(
         'enabled' => true,
-        'binary'  => "/usr/local/wkhtmlto/bin/wkhtmltoimage", //For linux
-        //'binary'  => "\"C:/Program Files/wkhtmltopdf/bin/wkhtmltoimage.exe\"", //For windows remove comments
+        //'binary'  => "/usr/local/wkhtmlto/bin/wkhtmltoimage", //For linux remove comments nd add coments to bellow line
+        'binary'  => "\"C:/Program Files/wkhtmltopdf/bin/wkhtmltoimage.exe\"", //For windows remove comments
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
