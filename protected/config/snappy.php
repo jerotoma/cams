b89be028fe65b6815108d47b338e3b759725d162
Innocent Christopher <chriss-inno@users.noreply.github.com>
2017-06-16 15:16:51 +0300

    Update snappy.php
    Update for ubuntu

--- protected/config/snappy.php
+++ protected/config/snappy.php
@@ -5,8 +5,8 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        //'binary'  => "/usr/local/wkhtmlto/bin/wkhtmltopdf", //For linux remove comments nd add coments to bellow line
-        'binary'  => "\"C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe\"", //For windows remove comments
+        'binary'  => "/usr/local/wkhtmlto/bin/wkhtmltopdf", //For linux remove comments nd add coments to bellow line
+        //'binary'  => "\"C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe\"", //For windows remove comments
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
