420fcb82bbabfda20bb990cbe2c9408bb5e53806
Otuman Nkomanya <otuman.nkomanya@athenasoftware.net>
2020-04-05 17:12:55 -0400

    Readded unused provider

--- protected/config/app.php
+++ protected/config/app.php
@@ -174,7 +174,7 @@ return [
          */
 
         //
-		Barryvdh\DomPDF\ServiceProvider::class,
+		//Barryvdh\DomPDF\ServiceProvider::class,
 		//Zizaco\Entrust\EntrustServiceProvider::class,
 		PrettyRoutes\ServiceProvider::class,
 		Collective\Html\HtmlServiceProvider::class,
@@ -240,7 +240,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 		//'Entrust'   => Zizaco\Entrust\EntrustFacade::class,
-		'DOMPDF' => Barryvdh\DomPDF\Facade::class,
 		'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
 		'Excel' => Maatwebsite\Excel\Facades\Excel::class,
